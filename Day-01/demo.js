ad43be8cce7912f9938ff913183e994d3f3d6109
Yaksh <yakshparikh2004@gmail.com>
2025-02-19 17:20:27 +0530

    Day-03 Update

---
+++ Day-01/demo.js
@@ -0,0 +1,14 @@
+let answer=true;
+let x=answer.opposite=false;
+let y = typeof(answer);
+console.log(x);
+console.log(answer.opposite);
+console.log(typeof(x));
+console.log(y);
+
+
+
+let str="abc";
+str[1]="F";
+console.log(str);
+console.log(str[1]);
